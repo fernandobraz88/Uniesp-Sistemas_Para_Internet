bbce5ca7bc8c16bfb56388b4c769b19f4fd70ae5
Fernando Braz da Silveira <112355109+fernandobraz88@users.noreply.github.com>
2023-03-08 15:56:21 -0300

    Add files via upload

---
+++ P2 - 2023.1/Front-End_II/Atividade 01/Atividade_01.js
@@ -0,0 +1,26 @@
+var i, altura, sexo, maiorAlt = 0, menorAlt = 99, mulher = 0, homem = 0, mediaMulher = 0, mediaHomem = 0, contMulher = 0, contHomem = 0;
+
+		for(i = 1 ;i <= 5; i++) {		
+			altura = parseFloat(prompt("Informe a altura (Utilize . não ,)["+i+"]:"));
+			sexo = prompt("Informe o sexo: M ou F:").toUpperCase();
+			
+
+			if(altura > maiorAlt) { 
+				maiorAlt = altura;
+			} else if(altura < menorAlt) { 
+				menorAlt = altura;
+
+			} if(sexo == 'F') {
+				mulher += altura;
+				contMulher++;
+			} else if(sexo == 'M') {
+				homem += altura;
+				contHomem++;
+			}
+		}
+		mediaMulher = mulher/contMulher;
+        mediaHomem = homem/contHomem;
+        document.writeln(`A maior altura é: <strong>${+maiorAlt}</strong> <br>`); 
+        document.writeln(`A menor altura é: <strong>${+menorAlt}</strong> <br>`);
+        document.writeln(`A média da altura dos homens é: <strong>${+mediaHomem.toFixed(2)}</strong> <br>`);
+        document.writeln(`O número de mulheres é: <strong>${+contMulher}</strong><br>`);
